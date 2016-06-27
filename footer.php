9347a7748a0ea9e464c7a7893bbedca77a0ceabf
Gabriel Vieira <gabriel@evolucaoagil.com.br>
2016-06-27 12:01:06 -0300

    New social icon at footer

--- footer.php
+++ footer.php
@@ -9,10 +9,12 @@
 
 		<nav class="social-networking">
 			<ul>
-				<li><a target="_blank" href="https://twitter.com/agilebrazil" class="social-icon social-icon--twitter">Twitter</a></li>
-				<li><a target="_blank" href="https://www.facebook.com/AgileBrazil" class="social-icon social-icon--facebook">Facebook</a></li>
-				<li><a target="_blank" href="https://www.linkedin.com/groups/Agile-Brazil-3148432" class="social-icon social-icon--linkedin">Linkedin</a></li>
-				<li><a target="_blank" href="https://vimeo.com/agilebrazil" class="social-icon social-icon--vimeo">Vimeo</a></li>
+				<li><a class="" href="https://www.facebook.com/AgileBrazil" target="_blank"><i class="fa fa-facebook-official  fa-4x"></i></a></li>
+				<li><a class="" href="https://twitter.com/agilebrazil" target="_blank"><i class="fa fa-twitter-square fa-4x"></i></a></li>
+				<li><a class="" href="http://buff.ly/1WZuvVO" target="_blank"><i class="fa fa-google-plus-square fa-4x"></i></a></li>
+				<li><a class="" href="https://instagram.com/agilebrazil" target="_blank"><i class="fa fa-instagram fa-4x"></i></a></li>
+				<li><a class="" href="https://www.linkedin.com/groups/Agile-Brazil-3148432" target="_blank"><i class="fa fa-linkedin-square  fa-4x"></i></a></li>
+				<li><a class="" href="https://vimeo.com/agilebrazil" target="_blank"><i class="fa fa-vimeo-square fa-4x"></i></a></li>
 			</ul>
 		</nav>
 	</div>
